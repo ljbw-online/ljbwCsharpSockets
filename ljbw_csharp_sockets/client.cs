793270f8c6ab83deb31369dae86def977b522644
ljbw <ljbw@DESKTOP-2F5HJOP>
2022-08-13 18:08:34 +0100

    Add project files.

---
+++ ljbw_csharp_sockets/client.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class GetSocket
+{
+    public static void Main()
+    {
+        IPEndPoint ipe = new IPEndPoint(IPAddress.Loopback, 7000);
+
+        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        sock.Connect(ipe);
+
+        Byte[] bytesToSend;
+
+        Console.WriteLine("Enter text to send through the socket. Enter \"quit\" to stop the program.");
+
+        while (true)
+        {
+            string? readLine = Console.ReadLine();
+
+            if (readLine != null)
+            {
+                bytesToSend = Encoding.ASCII.GetBytes(readLine);
+
+                sock.Send(bytesToSend);
+
+                if (readLine == "quit")
+                {
+                    sock.Shutdown(SocketShutdown.Both);
+                    sock.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ server/server.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+// This program listens for a connection on 127.0.0.1:7000. It can be used to test the socket client.
+
+Socket sock = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+sock.Bind(new IPEndPoint(IPAddress.Loopback, 7000));
+
+sock.Listen();
+
+int bytes;
+Byte[] bytesReceived = new Byte[256];
+string page;
+
+Console.WriteLine("Waiting for a connection");
+Socket clientSocket = sock.Accept();
+
+while (true)
+{
+    bytes = clientSocket.Receive(bytesReceived);
+
+    page = Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+
+    Console.WriteLine(page);
+
+    // It would be better to detect that the sending socket has been closed, and stop automatically, than require the user to enter "quit".
+    if (page == "quit")
+    {
+        Console.WriteLine("Quitting");
+        sock.Close();
+        clientSocket.Close();
+        break;
+    }
+}
